d367dcf953f2cf36940fc43ff28d4891af21490b
ahmad.taqiyudin@formulatrix.com <ahmad.taqiyudin@formulatrix.com>
2024-07-01 14:15:49 +0700

    Menit 40

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 import styles from "./categorylist.module.css"
+import Link from "next/link";
+import Image from "next/image";
 
 const CategoryList = () => {
     return (
-        <div className={styles.container}>CategoryList</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>Popular Categories</h1>
+            <div className={styles.categories}>
+                <Link href={"/blog?"} className={`${styles.category} ${styles.style}`}>
+                    <Image src={"/style.png"} alt={""} width={32} height={32} className={styles.image}/>
+                    Style
+                </Link>
+                <Link href={"/blog?"} className={`${styles.category} ${styles.style}`}>
+                    <Image src={"/fashion.png"} alt={""} width={32} height={32} className={styles.image}/>
+                    Fashion
+                </Link>
+                <Link href={"/blog?"} className={`${styles.category} ${styles.style}`}>
+                    <Image src={"/food.png"} alt={""} width={32} height={32} className={styles.image}/>
+                    Food
+                </Link>
+                <Link href={"/blog?"} className={`${styles.category} ${styles.style}`}>
+                    <Image src={"/travel.png"} alt={""} width={32} height={32} className={styles.image}/>
+                    Travel
+                </Link>
+                <Link href={"/blog?"} className={`${styles.category} ${styles.style}`}>
+                    <Image src={"/culture.png"} alt={""} width={32} height={32} className={styles.image}/>
+                    Culture
+                </Link>
+                <Link href={"/blog?"} className={`${styles.category} ${styles.style}`}>
+                    <Image src={"/coding.png"} alt={""} width={32} height={32} className={styles.image}/>
+                    Coding
+                </Link>
+            </div>
+        </div>
     )
 }
 
--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import styles from "./featured.module.css"
+import Image from "next/image";
 
 const Featured = () => {
     return (
-        <div className={styles.container}>Featured</div>
+        <div className={styles.container}>
+            <h1 className={styles.title}>
+                <b>Selamat Datang di teqo-tech: </b>Tutorial, Teknologi, Pikiran dan Inspirasi.
+            </h1>
+            <div className={styles.post}>
+                <div className={styles.imgContainer}>
+                    <Image src={"/p1.jpeg"} alt={""} fill className={styles.images}/>
+                </div>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.postTitle}>Ceritanya ini Judul artikel gituu</h1>
+                    <p className={styles.postDescription}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce id elit a velit malesuada
+                        rhoncus. Integer volutpat lorem vitae tristique malesuada. Maecenas imperdiet lobortis leo et
+                        congue. Vestibulum dapibus orci et nisl efficitur, eget mattis lectus eleifend. Quisque quis
+                        quam nisi. Vestibulum et turpis ut leo viverra iaculis sagittis eu erat. Cras vehicula tincidunt
+                        risus, lobortis maximus lectus laoreet ut.
+                    </p>
+                    <button className={styles.button}>Read More</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
